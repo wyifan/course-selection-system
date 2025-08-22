e4fd82ab911503ebbc36e909241a1b942ac9e6d8
shawn <wyifan@vip.qq.com>
2025-08-22 10:10:29 +0800

    增加日志打印当前目录

--- code-generater/src/main/java/com/yifan/code_generater/CodeGeneraterApplication.java
+++ code-generater/src/main/java/com/yifan/code_generater/CodeGeneraterApplication.java
@@ -8,11 +8,17 @@ import com.yifan.code_generater.executor.JsonTableLoader;
 import com.yifan.code_generater.executor.SqlExecutor;
 
 import com.yifan.code_generater.config.ConfigFromYml;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class CodeGeneraterApplication {
 
 	public static void main(String[] args) {
 		// SpringApplication.run(CodeGeneraterApplication.class, args);
+
+		// 假设当前目录就是 Spring Boot 项目根目录
+		String projectPath = System.getProperty("user.dir");
+		log.info("当前项目路径: {}", projectPath);
 
 		// 从 yml 读取配置
 		ConfigFromYml config = ConfigFromYml.loadFromYaml("setting.yml");
